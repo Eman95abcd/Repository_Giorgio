e14898a338f01a2cc327a57a9e9891eb651789e3
Eman95abcd <emanuele.kappa95@gmail.com>
2023-05-16 12:07:34 +0200

    esercizio 11 stringa di array

---
+++ esercizio_11/ArrayStr.java
@@ -0,0 +1,12 @@
+
+public class ArrayStr {
+    public static void main(String[] args){
+        String[] numeri = {"uno", "due", "tre", "quattro", "cinque"};
+        for (String num : numeri){
+           
+            System.out.println (num);
+        
+        }
+    }
+    
+}
